ec269b78fb04a672305da89b398dcfe2d8c4afbe
fanghenshaometeor <fanghenshao@sjtu.edu.cn>
2022-05-21 15:14:35 +0800

    update-training

--- train_benchmark.py
+++ train_benchmark.py
@@ -1,15 +1,21 @@
-import enum
-from sched import scheduler
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
 
+from torch.utils.tensorboard import SummaryWriter
+
+import os
+import sys
 import argparse
+
+import numpy as np
 
 from model import net_g_shallow
 from model import net_g_deep
 from model import GRFFNet
-from utils import AverageMeter, accuracy, setup_seed
+from utils import AverageMeter, accuracy
+from utils import setup_seed
+from utils import Logger
 from data_loader import get_dataloader
 
 # -------- fix data type ----------------
@@ -20,6 +26,9 @@ parser = argparse.ArgumentParser(description='Kernel Learning via multi-layer GR
 # -------- file param. --------------
 parser.add_argument('--data_folder',type=str,help='data folder')
 parser.add_argument('--dataset',type=str,help='data set name')
+parser.add_argument('--log_dir',type=str,default='./runs/',help='tensorboard')
+parser.add_argument('--model_dir',type=str,default='./save/',help='model save folder')
+parser.add_argument('--output_dir',type=str,default='./output/',help='terminal output')
 # -------- exp. settings ------------
 parser.add_argument('--batch_size',type=int,default=512,help='batch-size')
 parser.add_argument('--num_train',type=int,default=0,help='number of training set')
@@ -33,16 +42,30 @@ parser.add_argument('--ratio_val',type=float,default=0.2,help='ratio of validati
 parser.add_argument('--ratio_test',type=float,default=0.5,help='ratio of test')
 # -------- exp. settings ------------
 parser.add_argument('--num_repeat',type=int,default=5,help='number of repeat')
+parser.add_argument('--print_freq',type=int,default=10,help='frequency of print info.(epoch)')
 # -------- model settings -----------
 parser.add_argument('--num_layers',type=int,default=2,help='number of layers in GRFF')
-parser.add_argument('--D',nargs='+',type=int,default=[256, 64])
+parser.add_argument('--D',nargs='+',type=int,default=[256,64])
 parser.add_argument('--epochs',nargs='+',type=int,default=[1000,5000])
+# -------- training settings --------
+parser.add_argument('--lr',type=float,default=0.1,help='learning rate')
+parser.add_argument('--wd',type=float,default=5e-4,help='weight-decay')
 args = parser.parse_args()
+
+writer=SummaryWriter(os.path.join(args.log_dir,args.dataset+'/'))
+if not os.path.exists(os.path.join(args.model_dir,args.dataset)):
+    os.makedirs(os.path.join(args.model_dir,args.dataset))
+args.save_path=os.path.join(args.model_dir,args.dataset)
+if not os.path.exists(os.path.join(args.output_dir,args.dataset)):
+    os.makedirs(os.path.join(args.output_dir,args.dataset))
+args.output_path=os.path.join(args.output_dir,args.dataset,'train.log')
+sys.stdout = Logger(filename=args.output_path,stream=sys.stdout)
+args.data_folder='/home/dev/fangkun/data/UCI/'+args.dataset
 
 # -------- main function
 def main():
     setup_seed(666)
-
+    acctr_record, accte_record = np.zeros(args.num_repeat), np.zeros(args.num_repeat)
     # -------- Repeating 5 Times
     for repeat_idx in range(args.num_repeat):
 
@@ -72,20 +95,31 @@ def main():
 
         # -------- Progressively Training Freeze Inverse
         print("---- Start Progressively Training...")
-        prog_train(net)
-        # -------- Perform Training on the whole training set
+        chosen_accte=prog_train(net, trainloader, testloader, valloader, repeat_idx)
+    
+        # -------- Evaluation on the whole training set
         args.val=0
         trainloader, testloader, _ = get_dataloader(args)
         print("-------- --------")
-        print("---- Data information:")
         print("---- data set: ", args.dataset)
         print("---- # dimension = %d."%args.num_dim)
         print("---- # train/test/val. = %d/%d/%d"%(args.num_train, args.num_test, args.num_val))
-
+        ckpt = torch.load(os.path.join(args.save_path,"best-%d.pth"%repeat_idx),map_location=torch.device("cpu"))
+        net.load_state_dict(ckpt['state_dict'])
+        acctr=val(net, trainloader, None)
+        accte=val(net, testloader, None)
+        print("---- Repeat %d/%d: Train/Test acc.=%.2f/%.2f"%(repeat_idx+1,args.num_repeat,acctr,accte))
+        acctr_record[repeat_idx] = acctr
+        accte_record[repeat_idx] = accte
+    print("======== ========")
+    print("---- Avg. Results")
+    print("---- Training: %.2f %.2f; Test: %.2f %.2f."%(acctr_record.mean(), acctr_record.std(), accte_record.mean(), accte_record.std()))
+    
     return
 
 # -------- progressively training Freeze & Inverse
-def prog_train(net, trainloader):
+def prog_train(net, trainloader, testloader, valloader, repeat_idx):
+    best_acctr, best_accva, chosen_accte, best_epoch = 0, 0, 0, 0
     for layer_idx in range(args.num_layers):
         print('----------------')
         print('Training phase %d/%d...' % (layer_idx+1, args.num_layers))
@@ -93,15 +127,33 @@ def prog_train(net, trainloader):
         print('To-be-optimized params: ', opt_params_idx)
 
         # -------- preparing optimizer and scheduler
-        optimizer = optim.Adam(opt_params, lr=args.lr, weight_decay=args.weight_decay)
-        scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[args.sche_step], gamma=args.sche_gamma)
+        optimizer = optim.Adam(opt_params, lr=args.lr, weight_decay=args.wd)
 
         for epoch in range(args.epochs[layer_idx]):
+
             prog_train_epoch(net, trainloader, optimizer, epoch)
-            scheduler.step()
 
+            acctr=val(net, trainloader, epoch)
+            accte=val(net, testloader, epoch)
+            accva=val(net, valloader, epoch)
 
-    return
+            if acctr > best_acctr:
+                best_acctr = acctr
+            if accva > best_accva:
+                best_accva = accva
+
+                if acctr >= best_acctr:
+                    chosen_accte = accte
+                    best_epoch = epoch
+                    checkpoint = {'state_dict': net.state_dict()}
+                    torch.save(checkpoint, os.path.join(args.save_path,"best-%d.pth"%repeat_idx))
+
+            if epoch % args.print_freq == 0 or epoch == args.epochs[layer_idx]-1:
+                print('Updated at %d-epoch: train/test/val acc.=%.2f/%.2f/%.2f!' % (best_epoch, best_acctr, chosen_accte, best_accva))
+                print('Current    %d-epoch: train/test/val acc.=%.2f/%.2f/%.2f!' % (epoch, acctr, accte, accva))
+                print('-------------------------------------------------------')
+
+    return chosen_accte
 
 def get_opt_params(layer_idx, net):
     if layer_idx == (args.num_layers-1):
